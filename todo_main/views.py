87fc21f955557809fa4c4ba8abb5b75eaf1aeb55
Your Name <your@email.com>
2026-04-01 20:28:07 -0700

    todo app final version

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,7 +4,12 @@ from main_app.models import Task
 from django.shortcuts import render
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
+    completed_task = Task.objects.filter(is_completed=True)
+    print(completed_task)
     context = {
         "tasks": tasks,
+        "completed_task": completed_task,
+        
+
     }
     return render(request, 'home.html', context)
